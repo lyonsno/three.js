08de971131013d40e4794f67c0b268e29e666cf6
gonnavis <gonnavis@163.com>
2021-02-07 01:27:46 +0800

    a

--- src/renderers/shaders/ShaderLib/normal_frag.glsl.js
+++ src/renderers/shaders/ShaderLib/normal_frag.glsl.js
@@ -40,19 +40,24 @@ mat4 extractRotation( mat4 me ) {
 		0,0,0,1
 	);
 
-	te[0][ 0 ] = me[0][ 0 ];
-	te[0][ 1 ] = me[0][ 1 ];
-	te[0][ 2 ] = me[0][ 2 ];
+
+	float scaleX = 1. / length(vec3(me[0][0],me[0][1],me[0][2]));
+	float scaleY = 1. / length(vec3(me[1][0],me[1][1],me[1][2]));
+	float scaleZ = 1. / length(vec3(me[2][0],me[2][1],me[2][2]));
+
+	te[0][ 0 ] = me[0][ 0 ]*scaleX;
+	te[0][ 1 ] = me[0][ 1 ]*scaleX;
+	te[0][ 2 ] = me[0][ 2 ]*scaleX;
 	te[0][ 3 ] = 0.;
 
-	te[1][ 0 ] = me[1][ 0 ];
-	te[1][ 1 ] = me[1][ 1 ];
-	te[1][ 2 ] = me[1][ 2 ];
+	te[1][ 0 ] = me[1][ 0 ]*scaleY;
+	te[1][ 1 ] = me[1][ 1 ]*scaleY;
+	te[1][ 2 ] = me[1][ 2 ]*scaleY;
 	te[1][ 3 ] = 0.;
 
-	te[2][ 0 ]= me[2][ 0 ];
-	te[2][ 1 ]= me[2][ 1 ];
-	te[2][ 2 ] = me[2][ 2 ];
+	te[2][ 0 ]= me[2][ 0 ]*scaleZ;
+	te[2][ 1 ]= me[2][ 1 ]*scaleZ;
+	te[2][ 2 ] = me[2][ 2 ]*scaleZ;
 	te[2][ 3 ] = 0.;
 
 	te[3][ 0 ] = 0.;
@@ -71,14 +76,14 @@ void main() {
 	#include <normal_fragment_begin>
 	#include <normal_fragment_maps>
 
-	// mat4 rotationMatrix=extractRotation(viewMatrix);
-	mat4 rotationMatrix=mat4(
-		1,0,0,0,
-		0,1,0,0,
-		0,0,1,0,
-		0,0,0,1
-	);
-	rotationMatrix=inverse(rotationMatrix);
+	mat4 rotationMatrix=extractRotation(viewMatrix);
+	// mat4 rotationMatrix=mat4(
+	// 	1,0,0,0,
+	// 	0,1,0,0,
+	// 	0,0,1,0,
+	// 	0,0,0,1
+	// );
+	// rotationMatrix=inverse(rotationMatrix);
 	vec3 worldNormal=(rotationMatrix*vec4(normal,1)).xyz;
 	gl_FragColor = vec4( packNormalToRGB( worldNormal ), opacity );
 	// gl_FragColor = vec4( packNormalToRGB( normal ), opacity );
