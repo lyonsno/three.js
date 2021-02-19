d9da5d28dc2fe005720d75c60c3750e4acb14a5d
gonnavis <gonnavis@163.com>
2021-02-20 04:42:48 +0800

    SSRPassReflector

--- examples/jsm/objects/ReflectorForSSRPass.js
+++ examples/jsm/objects/ReflectorForSSRPass.js
@@ -255,36 +255,28 @@ Reflector.ReflectorShader = {
 		'}'
 	].join( '\n' ),
 
-	fragmentShader: [
-		'uniform vec3 color;',
-		'uniform sampler2D tDiffuse;',
-		'uniform sampler2D tDepth;',
-		'varying vec4 vUv;',
-
-		'float blendOverlay( float base, float blend ) {',
-
-		'	return( base < 0.5 ? ( 2.0 * base * blend ) : ( 1.0 - 2.0 * ( 1.0 - base ) * ( 1.0 - blend ) ) );',
-
-		'}',
-
-		'vec3 blendOverlay( vec3 base, vec3 blend ) {',
-
-		'	return vec3( blendOverlay( base.r, blend.r ), blendOverlay( base.g, blend.g ), blendOverlay( base.b, blend.b ) );',
-
-		'}',
-
-		'void main() {',
-
-		'	vec4 base = texture2DProj( tDiffuse, vUv );',
-		'	#ifdef useDepthTexture',
-		'		vec4 depth = texture2DProj( tDepth, vUv );',
-		'		gl_FragColor = vec4( blendOverlay( base.rgb, color ), pow(1.-depth.r,10./*temp*/) );',
-		'	#else',
-		'		gl_FragColor = vec4( blendOverlay( base.rgb, color ), 1.0 );',
-		'	#endif',
-
-		'}'
-	].join( '\n' )
+	fragmentShader: `
+		uniform vec3 color;
+		uniform sampler2D tDiffuse;
+		uniform sampler2D tDepth;
+		varying vec4 vUv;
+		float blendOverlay( float base, float blend ) {
+			return( base < 0.5 ? ( 2.0 * base * blend ) : ( 1.0 - 2.0 * ( 1.0 - base ) * ( 1.0 - blend ) ) );
+		}
+		vec3 blendOverlay( vec3 base, vec3 blend ) {
+			return vec3( blendOverlay( base.r, blend.r ), blendOverlay( base.g, blend.g ), blendOverlay( base.b, blend.b ) );
+		}
+		void main() {
+			vec4 base = texture2DProj( tDiffuse, vUv );
+			#ifdef useDepthTexture
+				float depth = texture2DProj( tDepth, vUv ).r;
+				if(depth>.01) discard;
+				gl_FragColor = vec4( blendOverlay( base.rgb, color ), pow(1.-depth,10./*temp*/) );
+			#else
+				gl_FragColor = vec4( blendOverlay( base.rgb, color ), 1.0 );
+			#endif
+		}
+	`,
 };
 
 export { Reflector };
