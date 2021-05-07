056007ca0e13d48e83d944f4b7b9132ab873d90d
gonnavis <gonnavis@163.com>
2021-05-07 12:31:40 +0800

    doing: pbr.

--- examples/jsm/shaders/SSRShader.js
+++ examples/jsm/shaders/SSRShader.js
@@ -206,17 +206,60 @@ var SSRShader = {
 						if(dot(viewReflectDir,vN)>=0.) continue;
 						float distance=pointPlaneDistance(vP,viewPosition,viewNormal);
 						if(distance>maxDistance) break;
-						float op=opacity;
-						#ifdef DISTANCE_ATTENUATION
-							float ratio=1.-(distance/maxDistance);
-							float attenuation=ratio*ratio;
-							op=opacity*attenuation;
-						#endif
-						#ifdef FRESNEL
-							float fresnelCoe=(dot(viewIncidentDir,viewReflectDir)+1.)/2.;
-							op*=fresnelCoe;
-						#endif
-						op*=metalness;
+
+						// float op=opacity;
+						// #ifdef DISTANCE_ATTENUATION
+						// 	float ratio=1.-(distance/maxDistance);
+						// 	float attenuation=ratio*ratio;
+						// 	op=opacity*attenuation;
+						// #endif
+						// #ifdef FRESNEL
+						// 	float fresnelCoe=(dot(viewIncidentDir,viewReflectDir)+1.)/2.;
+						// 	op*=fresnelCoe;
+						// #endif
+						// op*=metalness;
+
+						
+						vec3 N = viewNormal;
+						vec3 V = -viewIncidentDir;
+						float specularCoefficient = 0.16; // https://github.com/mrdoob/three.js/blob/9bbd837deb3e1d43d5c6c7054211eda5864bea06/src/renderers/shaders/ShaderChunk/lights_physical_pars_fragment.glsl.js#L18
+						vec3 F0 = vec3( specularCoefficient * ( reflectivity * reflectivity ) );
+						F0 = mix(F0, albedo, metallic); 
+
+						// calculate per-light radiance
+						vec3 L = normalize(lightPositions[i] - WorldPos);
+						vec3 H = normalize(V + L);
+						float distance = length(lightPositions[i] - WorldPos);
+						float attenuation = 1.0 / (distance * distance);
+						vec3 radiance = lightColors[i] * attenuation;
+
+						// Cook-Torrance BRDF
+						float NDF = DistributionGGX(N, H, roughness);   
+						float G   = GeometrySmith(N, V, L, roughness);      
+						vec3 F    = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
+							
+						vec3 nominator    = NDF * G * F; 
+						float denominator = 4. * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
+						vec3 specular = nominator / max(denominator, 0.001); // prevent divide by zero for NdotV=0.0 or NdotL=0.0
+						
+						// kS is equal to Fresnel
+						vec3 kS = F;
+						// for energy conservation, the diffuse and specular light can't
+						// be above 1.0 (unless the surface emits light); to preserve this
+						// relationship the diffuse component (kD) should equal 1.0 - kS.
+						vec3 kD = vec3(1.0) - kS;
+						// multiply kD by the inverse metalness such that only non-metals 
+						// have diffuse lighting, or a linear blend if partly metal (pure metals
+						// have no diffuse light).
+						kD *= 1.0 - metallic;	  
+
+						// scale light by NdotL
+						float NdotL = max(dot(N, L), 0.0);        
+
+						// add to outgoing radiance Lo
+						vec3 Lo = (kD * albedo / PI + specular) * radiance * NdotL;  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
+
+
 						vec4 reflectColor=texture2D(tDiffuse,uv);
 						gl_FragColor.xyz=reflectColor.xyz;
 						gl_FragColor.a=op;
